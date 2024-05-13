07e2bbedd56a7c200b5c951b192573d1cf37f040
TCB <tbinh2890@gmail.com>
2024-05-13 09:51:20 +0700

    first commit

---
+++ client/src/configs/ui.configs.js
@@ -0,0 +1,44 @@
+const uiConfigs = {
+    style: {
+        gradientBgImage: {
+            dark: {
+                backgroundImage: "Linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0))"
+            },
+            light: {
+                backgroundImage: "Linear-gradient(to top, rgba(245,245,245,1), rgba(0,0,0,0))"
+            }
+        },
+        horizontalGradientBgImage: {
+            dark: {
+                backgroundImage: "Linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0))"
+            },
+            light: {
+                backgroundImage: "Linear-gradient(to top, rgba(245,245,245,1), rgba(0,0,0,0))"
+            }
+        },
+        typoLines: (lines, textAlign) => ({
+            textAlign: textAlign || "justify",
+            display: "-webkit-box",
+            overflow: "hidden",
+            WebkitBoxOrient: "vertical",
+            WebkitLineClamp: lines
+        }),
+        mainContent: {
+            maxWidth: "1366px",
+            margin: "auto",
+            padding: 2
+        },
+        backgroundImage: (imgPath) => ({
+            position: "relative",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundColor: "darkgrey",
+            backgroundImage: `url(${imgPath})`
+        })
+    },
+    size: {
+        sizebarWith: "300px",
+        contentMaxWidth: "1366px"
+    }
+}
+export default uiConfigs
